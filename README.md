cc3581952143072ee5943559988266b0b7b38c66
Henrik Loeser <hloeser@de.ibm.com>
2021-03-22 09:00:43 +0100

    enhancements

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# twitterBot
+# TtwitterBot for IBM Cloud Code Engine
 Some fun and experiments with Golang, Twitter and IBM Cloud Code Engine
+
+
+## (Rough) Instructions
+
+1. Set up Code Engine (CE) project
+2. create registry in CE
+3. configure .env with credentials / secrets
+4. create secret from file in CE
+5. build the container image, either in CE or using the Container Registry
+6. create the CE app from the image and pass the configured secrets / credentials
+7. set up the CE ping subscription and pass the secret key
--- twitterBot.go
+++ twitterBot.go
@@ -1,3 +1,11 @@
+// Simple Twitter bot for testing
+// IBM Cloud Code Engine subscriptions.
+// The bot runs as http server (Code Engine app) and receives
+// POST requests on the "tweet" route. The POST requests are
+// sent be the ping subscription at the configured times.
+//
+// Written by Henrik Loeser, 2021
+
 package main
 
 import (
@@ -14,15 +22,19 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 )
 
+// Variables, taken from .env or K8s secrets
 var (
 	TwitterAPIKey            string = os.Getenv("TWITTER_APIKEY")
 	TwitterAPISecret         string = os.Getenv("TWITTER_APIKEY_SECRET")
 	TwitterAccessToken       string = os.Getenv("TWITTER_ACCESS_TOKEN")
 	TwitterAccessTokenSecret string = os.Getenv("TWITTER_ACCESS_TOKEN_SECRET")
+	SecretKey                string = os.Getenv("SECRET_KEY")
 )
 
+// run the http server with two routes:
+// 1) /:       Hello world
+// 2) /tweet:  Send the Twitter status update (tweet)
 func main() {
-
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
@@ -32,21 +44,29 @@ func main() {
 
 }
 
+// Receive the request to tweet. First check for the passed secret.
+// If it matches, proceed to set up the Twitter client and post the
+// status update.
 func tweet(c echo.Context) error {
-	log.Printf("tweet - Key %s", TwitterAPIKey)
-	config := oauth1.NewConfig(TwitterAPIKey, TwitterAPISecret)
-	token := oauth1.NewToken(TwitterAccessToken, TwitterAccessTokenSecret)
-	// http.Client will automatically authorize Requests
-	httpClient := config.Client(oauth1.NoContext, token)
+	PassedSecret := c.FormValue("SECRET_KEY")
+	if SecretKey == PassedSecret {
+		config := oauth1.NewConfig(TwitterAPIKey, TwitterAPISecret)
+		token := oauth1.NewToken(TwitterAccessToken, TwitterAccessTokenSecret)
+		// http.Client will automatically authorize Requests
+		httpClient := config.Client(oauth1.NoContext, token)
 
-	// twitter client
-	client := twitter.NewClient(httpClient)
-	message := fmt.Sprintf("I am running on #IBMCloud #CodeEngine using #Golang and testing #event subscriptions. The time is %s", time.Now())
-	tweet, _, err := client.Statuses.Update(message, nil)
-	if err != nil {
-		log.Fatal(TwitterAPIKey)
-		return err
+		// twitter client
+		client := twitter.NewClient(httpClient)
+		message := fmt.Sprintf("I am running on #IBMCloud #CodeEngine using #Golang and testing #event subscriptions. The time is %s", time.Now())
+		tweet, _, err := client.Statuses.Update(message, nil)
+		if err != nil {
+			log.Fatal(TwitterAPIKey)
+			return err
+		}
+		// low level debugging for the logs... :)
+		fmt.Printf("STATUSES SHOW:\n%+v\n", tweet)
+		return c.String(http.StatusOK, "tweeted :)")
+	} else {
+		return c.String(http.StatusUnauthorized, "No matching secret provided")
 	}
-	fmt.Printf("STATUSES SHOW:\n%+v\n", tweet)
-	return c.String(http.StatusOK, "tweeted :)")
 }
